e657dcfd4814f876c6613842b6f3805e8dee804b
Kundan <ntamsoy662gmail.com>
2023-05-14 15:52:01 +0530

    added functionalities

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 let output = document.getElementsByTagName('p')[0];
 
-let plus = document.getElementById('plus');
-let minus = document.getElementById('minus');
-let multi = document.getElementById('multi');
-let divison = document.getElementById('divison');
 let equal = document.getElementById('equal');
 
 let fnum=''
 let sign = true
+let result = 0
+let rnum
 function concat(num) {
-    if (sign) {
-        fnum = fnum + num
-        output.innerText = fnum;
-    }else{
-        fnum = fnum + num
-        output.innerText = '-' +fnum;
-
+    // rnum = parseInt(num)
+    fnum = fnum + num
+    output.innerText = fnum;
+}
+function expression(oper,final) {
+    if (oper.includes('+')) {
+        result = parseInt(final[0]) + result + parseInt(final[1]);
+    }else if (oper.includes('-')) {
+        result = parseInt(final[0]) - parseInt(final[1]) - result;
+        plusMin()
+    }else if (oper.includes('x')){
+        if (!result) {
+            result = parseInt(final[0]) * parseInt(final[1]) * 1;
+        }else{
+            result = parseInt(final[0]) * parseInt(final[1]) * result;
+        }
+    }else if (oper.includes('/')) {
+        if (!result) {
+            result = (parseInt(final[0]) / parseInt(final[1])) / 1;
+        }else{
+            result = (parseInt(final[0]) / parseInt(final[1])) / result;
+        }
     }
+    // fnum = fnum + oper
+    // output.innerText = fnum;
+    // operator = oper
 }
 function empty(){
     fnum=''
     output.innerText = '0';
+    result=0
 }
 function plusMin(){
     if (sign) {
@@ -34,5 +51,19 @@ function plusMin(){
 function percent() {
     fnum = fnum / 100;
     output.innerText = fnum;
-    
+}
+equal.addEventListener('click',()=>{
+
+    let final = fnum.split(/[+-/x]/)
+    expression(fnum,final)
+    fnum = result
+    result=0
+    output.innerText = fnum;
+    console.log(fnum)
+})
+function clr() {
+    let len = fnum.length;
+    fnum = fnum.slice(0,len-1)
+    output.innerText = fnum;
+
 }
